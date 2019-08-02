a4a87f65eb7ac368a8cf522669c0393612ae583d
Laila Fitriana <lailafitriana@me.com>
2019-08-02 17:46:18 -0400

    first commit

---
+++ src/main/java/base22/SortingFile.java
@@ -0,0 +1,38 @@
+package base22;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortingFile {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		File unsortedIncomingFile = new File("unsorted.rtf");
+
+		File sortedFile = new File("sorted.txt");
+
+		@SuppressWarnings("resource")
+		PrintWriter printWriter = new PrintWriter(sortedFile.getAbsoluteFile());
+
+		@SuppressWarnings("resource")
+		Scanner incoming = new Scanner(unsortedIncomingFile);
+
+		String[] unsortedStrings = new String[25];
+
+		while (incoming.hasNextLine()) {
+			unsortedStrings = incoming.nextLine().split(" ");
+		}
+
+		Arrays.sort(unsortedStrings, (a, b) -> Integer.compare(a.length(), b.length()));
+
+		for (int i = 0; i < unsortedStrings.length; i++) {
+			printWriter.write(unsortedStrings[i] + "\n");
+		}
+		
+		printWriter.flush();
+
+	}
+
+}
